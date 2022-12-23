c685d7d8df470a1c0fdf0942b25e00fb794a85d6
Anurag Mishra <anurago.asm@gmail.com>
2022-12-23 00:10:19 -0800

    refactor and cleanup

--- main.py
+++ main.py
@@ -13,16 +13,22 @@ class GetTeamLists():
             print('Series: {}, {} vs. {}'.format(match.match_title,
                                                  match.team_1_abbreviation,
                                                  match.team_2_abbreviation))
-            self.printPlayerList(match.team_1_abbreviation,
-                                 match.team_1_players)
-            self.printPlayerList(match.team_2_abbreviation,
-                                 match.team_2_players)
+            self.printPlayerList(match)
 
-    def printPlayerList(self, team, playerList):
-        print('{}:'.format(team))
-        for player in playerList:
-            print('{}'.format(player['card_long']))
-        print('')
+    def printPlayerList(self, match):
+        print('{}:'.format(match.match_title))
+
+        def extractPlayerNames(playerList):
+            for player in playerList:
+                print('{}'.format(player['card_long']))
+            print()
+
+        # Team 1
+        print('{}:'.format(match.team_1_abbreviation))
+        extractPlayerNames(match.team_1_players)
+        # Team 2
+        print('{}:'.format(match.team_2_abbreviation))
+        extractPlayerNames(match.team_2_players)
 
 
 # Will fail for cases where a team isn't found at the moment
